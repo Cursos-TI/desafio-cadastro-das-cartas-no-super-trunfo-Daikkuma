60df6e639d84067fc14fd8c873e8203443fade5b
Daikkuma <daigremory@hotmail.com>
2025-08-25 16:12:58 +0000

    criei a carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,21 +4,67 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
 
 int main() {
+    // mensagens iniciais
     printf ("Desafio Super Trunfo - Países\n");
+    printf ("Carta 1:\n");
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Declarar as variaveis
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado;
+    char codigo [50], nome_da_cidade [50];
+    int populacao;
+    float area;
+    float pib;
+    int numero_de_pontos_turisticos;
 
-    return 0;
+   
+    // Solicitar dados ao usuario com prinf e armazenar dados usando especificador de variaveis
+    // char [] contém string, onde já tem o endereco de memoria e & é usado para variáveis que guardam um único valor. 
+    //Isso inclui números (int, float) e um único caractere (char).
+
+    printf ("Digite uma letra de A-H representando um dos oito estados: ");
+    scanf (" %c", &estado);
+
+    printf ("Digite a letra do estado seguida de um numero de 01 a 04: ");
+    scanf ("%s", codigo);
+    
+    printf ("Digite o nome da Cidade: ");
+    scanf ("%s", nome_da_cidade);
+
+    printf ("Digite o numero de habitantes: ");
+    scanf (" %d", &populacao); //Adicionar espaco antes do % para prevenir erros de ``lixo de memoria``
+    
+    printf ("Digite a área em km2: ");
+    scanf (" %f", &area);
+
+    printf ("Digite o PIB: ");
+    scanf (" %f", &pib);
+
+    printf ("Digite o número de pontos turisticos: ");
+    scanf (" %d", &numero_de_pontos_turisticos);
+    printf ("\n");
+
+
+
+    /* Exibição dos dados cadastrados com prinf de forma organizada e clara
+     printf = mostrar na tela, ("Estado: %s\n") = Frase que vai aparecer junto com oque foi digitado e pulando 1 linha, "estado" = a descricao da variavel
+    que cadastrei no topo
+    */
+
+    printf ("Carta 1:\n");
+    printf ("Estado: %c\n", estado);
+    printf ("Código da Carta: %s\n", codigo);
+    printf ("Nome da Cidade: %s\n", nome_da_cidade);
+    printf ("População: %d\n", populacao);
+    printf ("Área: %f km²\n", area);
+    printf ("PIB: %.2f bilhões de reais\n", pib);
+    printf ("Número de Pontos Turísticos: %d\n", numero_de_pontos_turisticos);
+    printf ("\n"); // pulando linha
+
+        
+
+    return 0; // fim do programa
 }
